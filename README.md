d8ff96495d46afac667a1d13edc30fb9401cdd08
plord12 <peter@plord.co.uk>
2021-07-03 16:16:35 +0100

    Updates

--- README.md
+++ README.md
@@ -2,4 +2,42 @@
 
 Small collection of script to download pension plan values.
 
+I run these peridocally in a wrapper script and send the options to google drive - 
+a google sheet can then read these values for further processing.
+
+## Requirements
+
+* Puppeteer - see https://pptr.dev/
+* jq ( for netmeg.sh ) - see https://stedolan.github.io/jq/
+* curl ( for nutmeg.sh ) - see https://curl.se/
+
+## Scripts
+
+### aviva.js
+
+Gets an Aviva pension plan value.  Usage :
+
+    AVIVA_USER=myuser AVIVA_PWD=mypassword node aviva.
+
+### moneyfarm.js
+
+Gets a Monefarm pension plan value.  Usage :
+
+    MONEYFARM_USER=myuser MONEYFARM_PWD=mypassword MONEYFARM_TYPE=isa node moneyfarm.js
+
+### nutmeg.js
+
+Gets a Nutmeg pension plan value.  Usage :
+
+    NUTMEG_USER=myuser NUTMEG_PWD=mypassword NUTMEG_NAME="My pension" node nutmeg.js
+
+### nutmeg.sh
+
+Gets a Nutmeg pension plan value via the API.  Usage :
+
+    NUTMEG_USER=myuser NUTMEG_PWD=mypassword NUTMEG_NAME="My pension" ./nutmeg.sh
+
+
+
+
 
---
+++ nutmeg.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+#
+# NUTMEG_USER=myuser NUTMEG_PWD=mypassword NUTMEG_NAME="My pension" ./nutmeg.sh
+#
+
+jar=/tmp/nutmeg$$
+
+auth=$(curl --silent --request POST --data "{\"email\": \"${NUTMEG_USER}\", \"password\": \"${NUTMEG_PWD}\"}" --cookie ${jar} --cookie-jar ${jar} --header "Content-Type: application/json"  "https://api.nutmeg.com/users/authenticate")
+uuid=$(echo ${auth} | jq --raw-output '.uuid')
+token=$(echo ${auth} | jq --raw-output '.token')
+
+pots=$(curl --silent --cookie ${jar} --cookie-jar ${jar} --header "AUTH-TOKEN: Bearer ${token}" --header "API-CLIENT: WEB" --header "API-CONSUMER: MOBILE" "https://api.nutmeg.com/customers/${uuid}/pots")
+
+# echo $pots | jq .
+
+for encodedpot in $(echo "${pots}" | jq  --raw-output '.pots[] | @base64')
+do
+	pot=$(echo ${encodedpot} | base64 --decode)
+	if [ "${NUTMEG_NAME}" == "$(echo "${pot}" | jq --raw-output '.name')" ]
+	then
+		currentValue=$(echo "${pot}" | jq --raw-output '.currentValue')
+		pendingValue=$(echo "${pot}" | jq --raw-output '.pendingValue')
+		echo "${currentValue}+${pendingValue}" | bc
+	fi
+done
+
+rm -f ${jar}
